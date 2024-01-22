44455d264f0a5d3b54608df2b37896caa48fda98
YacineMerhoum <yacinemerhoum@gmail.com>
2024-01-22 10:53:04 +0100

    first commit

---
+++ htdocs/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotify</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<header>
+<nav class="navbar  nav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php" alt="menu">
+        <img class="logo" src="./images/spotify.png">
+    </a>
+
+    <form class="d-flex" action="" method="post">
+      <!-- inscription  -->
+      <button class="btn btn-outline fs-5 " type="submit">S'inscrire</button>
+    </form>
+
+
+    <form class="d-flex" action="" method="post">
+      <!-- connexion session  -->
+      <button type="button" class="btn btn-light fs-5">Connexion</button>
+
+    </form>
+  </div>
+</nav>
+</header>
+
+<body>
+   
+
+
+
+    
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
