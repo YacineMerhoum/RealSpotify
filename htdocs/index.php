e94885a8f35ace3ae793464b375157a3d0f82c9b
YacineMerhoum <yacinemerhoum@gmail.com>
2024-01-23 14:23:09 +0100

    mardiaprem

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "./config/verif_superglobal.php";
+// include "./config/verif_superglobal.php";
 require_once "./config/connexion.php";
 ?>
 
@@ -21,20 +21,41 @@ require_once "./config/connexion.php";
         <img class="logo" src="./images/spotify.png">
       </a>
 
-      <a href="./register.php">
+
+
+      <?php if (!empty($_SESSION["pseudo"])){
+
+      }else{
+      echo'<a href="./register.php">
         <!-- inscription  -->
-        <button class="btn btn-outline fs-5 subscribe " type="submit">S'inscrire</button>
-      </a>
+        <button class="btn btn-outline fs-5 subscribe " type="submit">S\'inscrire</button>
+      </a>';  }; ?>
 
-  <form action="./process/logout.php" method="post">
-          <button type="submit">deconnexion</button>
-  </form>
 
-      <a href="./pagelogin.php">
-        <!-- connexion session  -->
-        <button type="submit login" class="btn btn-light fs-5 login">Connexion</button>
+  <!-- le bouton est a mettre dans une condition if  -->
+  <?php if (!empty($_SESSION["pseudo"])){
+  echo '<form action="./process/logout.php" method="post">
+          <button type="submit"  class="btn btn-outline fs-5 subscribe ">Déconnexion</button>
+  </form>';
+  }?>
+  
 
-      </a>
+<!-- condition if pour la connexion également  -->
+  <?php
+if (!empty($_SESSION["pseudo"])) {
+
+} else {
+    echo '<a href="./pagelogin.php">
+            <!-- connexion session  -->
+            <button type="submit" class="btn btn-light fs-5 login">Connexion</button>
+          </a>';
+}
+?>
+
+
+
+
+
     </div>
   </nav>
 </header>
@@ -45,9 +66,9 @@ require_once "./config/connexion.php";
 
     <div class="col-2 col-md-2">
       <div class="d-grid col-6 ">
-        <button class="btn btn-flex mt-3" type="button"><a href="./index.php">Accueil</a></button>
-        <button class="btn btn-flex" type="button"><a href="">Rechercher</a></button>
-        <button class="btn btn-flex mt-1" type="button"><a href="">Bibiliothèque</a></button>
+        <button class="btn btn-flex mt-3 text-start" type="button"><a href="./index.php"><i class="fa-solid fa-house" style="color: #ffffff;"></i> Accueil</a></button>
+        <button class="btn btn-flex text-start" type="button"><a href=""><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i> Rechercher</a></button>
+        <button class="btn btn-flex mt-1 text-start" type="button"><a href=""><i class="fa-solid fa-book-open" style="color: #ffffff;"></i> Bibiliothèque</a></button>
         <div class="test">
           <div class="inside ">
             <button class="btn btn-flex mt-5 btninside" type="button"><a href="">Créer une playlist</a></button>
@@ -66,11 +87,84 @@ require_once "./config/connexion.php";
 
     <!-- grosse partie droite  Le code doit se passer ici !! -->
     <div class="col-sm-10 col-md-10">
-      <div class="secteur">
+      <div class="secteur"> 
         <br>
-        <p class="text-white fs-3">Connectez-vous pour accéder à vos Playlists</p>
+        
         <br>
+        <?php if (!empty($_SESSION["pseudo"])) {
+            echo '<p class="text-white fs-3">' . "Bienvenue " .  $_SESSION["pseudo"] . " dans votre univers musical " .
+               '</p>';
+           } else {
+           // Afficher autre chose ou ne rien afficher si la session n'est pas ouverte
+         echo '<p class="text-white fs-3">Connectez-vous pour accéder à vos Playlists</p>';
+         }; ?>
+         </p>
+         <br>
         <!-- flex box à l'interieur de la section  -->
+        
+        <div class="">
+        <div id="message" class="text-white fs-2"><i class="fa-solid fa-spinner fa-spin-pulse fa-2xl" style="color: #ffffff;"></i></div>
+        </div>
+        <br>
+        
+
+        <!-- test pour les flex box musicales  -->
+        <div class="d-flex justify-content-between">
+                  <div class="p-2 flex ms-4">Flex item 1
+                    <!-- div a l'intérieur  -->
+                    <div class="flexy">
+                      
+                    </div>
+
+
+
+                  </div>
+                  <div class="p-2 flex">Flex item 2</div>
+                  <div class="p-2 flex">Flex item 3</div>
+                  <div class="p-2 flex">Flex item 4</div>
+                  <div class="p-2 flex">Flex item 5</div>
+                  <div class="p-2 flex me-4">Flex item 6</div>
+      
+                </div>
+              </div>
+
+                
+                
+                <!-- lecteur footer -->
+              <section class="lecteur d-flex justify-content-center">
+              <!-- /* pochette et titre dans le lecteur a gauche  */ -->
+                  <div class="mt-5 content">Pochette album</div>
+
+
+
+
+                  <div class="mt-4 play p-3 "><i class="fa-solid fa-shuffle fa-2xl" style="color: #ffffff;"></i></div>
+                  <div class="mt-4 play p-3"><i class="fa-solid fa-backward fa-2xl" style="color: #ffffff;"></i></div>
+                  <div class="mt-4 play p-3"> <i class="fa-solid fa-play fa-2xl" style="color: #ffffff;"> </i></div>
+                  
+                  <div class="mt-4 play p-3"><i class="fa-solid fa-forward fa-2xl" style="color: #ffffff;"></i></div>
+
+                
+                  </section>
+             </div>
+           </div>
+
+           
+      
+    <!-- lecteur audio footer  -->
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+        
 
 
 
@@ -87,9 +181,6 @@ require_once "./config/connexion.php";
 
 
 
-      </div>
-    </div>
-  </div>
 
 
 
@@ -97,20 +188,10 @@ require_once "./config/connexion.php";
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/23471b5a81.js" crossorigin="anonymous"></script> 
+  <script src="./js/index.js"></script>
 </body>
 
 </html>
---
+++ htdocs/js/index.js
@@ -0,0 +1,11 @@
+// test texte 
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      
+      let messageElement = document.getElementById("message");
+
+      messageElement.innerHTML = 'Laissez-vous emporter par <span class="green">Spotify</span>';
+      
+      
+    }, 2000); 
+  });
